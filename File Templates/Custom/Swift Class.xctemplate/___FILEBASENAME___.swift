ad152a00a2c4de577d24b7e9c3e93cca8b8f4bee
Hai Nguyen <aquarchitecture@gmail.com>
2016-03-08 16:17:08 -0800

    Fixed Swift Class template

--- File Templates/Custom/Swift Class.xctemplate/___FILEBASENAME___.swift
+++ File Templates/Custom/Swift Class.xctemplate/___FILEBASENAME___.swift
@@ -1,9 +1,9 @@
 //
-//	___FILENAME___
-//	___PROJECTNAME___
+//  ___FILENAME___
+//  ___PROJECTNAME___
 //
-//	Created by ___FULLUSERNAME___ on ___DATE___.
-//	___COPYRIGHT___
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
 //
 
 final class ___FILEBASENAMEASIDENTIFIER___: <#SuperClass#> {
