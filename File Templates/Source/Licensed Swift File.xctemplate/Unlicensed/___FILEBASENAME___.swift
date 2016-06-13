6f97685d505b5f060a706ca1779145025b6df3ce
Hai Nguyen <aquarchitecture@gmail.com>
2016-06-12 17:37:33 -0700

    Finalized file templates

---
+++ File Templates/Source/Licensed Swift File.xctemplate/Unlicensed/___FILEBASENAME___.swift
@@ -0,0 +1,12 @@
+/*
+ * ___FILENAME___
+ * ___PROJECTNAME___
+ *
+ * Created by ___FULLUSERNAME___ on ___DATE___
+ * Copyright (c) ___YEAR___ ___FULLUSERNAME___. All rights reserved.
+ *
+ */
+
+<#Type#> ___FILEBASENAMEASIDENTIFIER___ {
+
+}
