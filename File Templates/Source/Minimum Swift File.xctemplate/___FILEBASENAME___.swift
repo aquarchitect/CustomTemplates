de96f77f1a2acd25e1d6446978e29ae6bcfe86e3
Hai Nguyen <aquarchitecture@gmail.com>
2017-04-26 22:34:44 -0700

    Fix source code comments

--- File Templates/Source/Minimum Swift File.xctemplate/___FILEBASENAME___.swift
+++ File Templates/Source/Minimum Swift File.xctemplate/___FILEBASENAME___.swift
@@ -1,7 +1,7 @@
-/*
- * ___FILENAME___
- * ___PROJECTNAME___
- *
- * Created by ___FULLUSERNAME___ on ___DATE___.
- * Copyright (c) ___YEAR___ ___FULLUSERNAME___.
- */
+//
+// ___FILENAME___
+// ___PROJECTNAME___
+//
+// Created by ___FULLUSERNAME___ on ___DATE___.
+// Copyright (c) ___YEAR___ ___FULLUSERNAME___.
+//
